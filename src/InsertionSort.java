47da0818c5922e5a242a4fe6a8afc1adc9f74f29
153091 <xpoint06@gmail.com>
2020-06-03 20:21:29 +0300

    i=1

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -13,10 +13,9 @@ public class InsertionSort {
 
     public static void sort(Comparable[] a) {
         int n = a.length;
-        for (int i = 0; i<n; i++)
+        for (int i = 1; i<n; i++)
             for (int j = i; j>0; j--)
                 if (less(a[j], a[j-1])) exch(a , j, j-1);
-                else break;
         assert isSorted(a);
     }
     private static void exch(Comparable[] a, int i, int j) {
